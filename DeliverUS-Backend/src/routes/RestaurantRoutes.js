85760b94b54e8395d4f8a19ca9a9f840c3cddbde
alevelmol <alevelmol@alum.us.es>
2024-03-08 19:13:00 +0100

    me queo sin betarua

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,6 +1,10 @@
 import OrderController from '../controllers/OrderController.js'
 import ProductController from '../controllers/ProductController.js'
 import RestaurantController from '../controllers/RestaurantController.js'
+import { hasRole, isLoggedIn } from '../middlewares/AuthMiddleware.js'
+import { checkEntityExists } from '../middlewares/EntityMiddleware.js'
+import { handleFilesUpload } from '../middlewares/FileHandlerMiddleware.js'
+import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
 
 const loadFileRoutes = function (app) {
   app.route('/restaurants')
@@ -8,20 +12,41 @@ const loadFileRoutes = function (app) {
       RestaurantController.index)
     .post(
     // TODO: Add needed middlewares
+      isLoggedIn,
+      hasRole('owner'),
+      handleFilesUpload(['image'], process.env.RESTAURANT_FOLDER),
+      RestaurantValidation.create,
+      handleValidation,
+      RestaurantMiddleware.checkRestaurantOwnership,
       RestaurantController.create)
 
-  app.route('/restaurants/:restaurantId')
-    .get(RestaurantController.show)
+  app.route('/restaurants/:restaurantId') // pa ve un restaurante
+    .get(
+      checkEntityExists(Restaurant, 'restaurantId'),
+      RestaurantController.show)
     .put(
     // TODO: Add needed middlewares
+      isLoggedIn,
+      hasRole('owner'),
+      checkEntityExists(Restaurant, 'restaurantId'),
+      RestaurantValidation.checkRestaurantOwnership,
+      handleFilesUpload(['logo','heroimage'], process.env.RESTAURANT_FOLDER),
+      RestaurantValidation.update,
+      handleValidation,
       RestaurantController.update)
     .delete(
     // TODO: Add needed middlewares
+      isLoggedIn,
+      hasRole('owner'),
+      checkEntityExists(Restaurant, 'restaurantId'),
+      RestaurantMiddleware.checkRestaurantOwnership,
+      RestaurantMiddleware.restaurantHasNoOrders,
       RestaurantController.destroy)
 
   app.route('/restaurants/:restaurantId/orders')
     .get(
     // TODO: Add needed middlewares
+      checkEntityExists(Restaurant, 'restaurantId'),
       OrderController.indexRestaurant)
 
   app.route('/restaurants/:restaurantId/products')
